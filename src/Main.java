59aa1f774ceae5662d4edee3c3ca8a6f8d759676
Igor Ostroverkhov <ostrovvetra@gmail.com>
2021-07-29 01:14:54 +0300

    edite proga

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,30 @@ public class Main {
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
         // суммы пополнения и тп
-        int initialInvoice = 100;
+        int invoice = 100;
         int tranche = 1150;
+        int x = 1000;
 
-        int bonus = tranche / 100;
-        int total = bonus + tranche + initialInvoice;
-        // Условным оператором проверяете превысила ли
-        // сумма пополнения порог и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
-        System.out.println("bonus = ");
-        System.out.println(bonus);
-        System.out.println("_______________________________");
-        System.out.println("total = ");
-        System.out.println(total);
+//boolean trancheReal = true;
+        if (x > tranche) {
+
+            int total = tranche + invoice;
+        } else {
+            int bonus = tranche / 100;
+
+
+            int total = bonus + tranche + invoice;
+            // Условным оператором проверяете превысила ли
+            // сумма пополнения порог и для этих двух разных
+            // сценариев рассчитываете сумму бонуса и выводите
+            // на экран.
+            System.out.println("bonus = ");
+            System.out.println(bonus);
+            System.out.println("_______________________________");
+            System.out.println("total = ");
+            System.out.println(total);
+
     }
+
+}
 }
